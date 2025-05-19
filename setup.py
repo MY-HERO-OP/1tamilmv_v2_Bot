6a2c80635b1ac1a19b4856d74747230d62da2dd5
MY-HERO-OP <129320317+MY-HERO-OP@users.noreply.github.com>
2025-05-19 18:24:45 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="tamilmv-bot",
+    version="0.1.0",
+    description="Telegram bot for downloading from Tamilmv",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="MY-HERO-OP",
+    author_email="heron3477@gmail.com",
+    url="https://github.com/MY-HERO-OP/1tamilmv_v2_Bot",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
